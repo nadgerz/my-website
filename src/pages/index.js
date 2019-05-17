0fe0a331f24e3e8ed27433aadcaa7f246d17de04
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-05-17 15:32:46 +0200

    janky

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,10 @@ const IndexPage = () => {
   return (
     <div>
       <h1>Hello.</h1>
-      <h1>I'm Steve, a fat bastard.</h1>
+      <h2>I'm Steve, a fat bastard.</h2>
+      <p>
+        Need a developer? <a href="/contact">Contact me.</a>
+      </p>
     </div>
   )
 }
