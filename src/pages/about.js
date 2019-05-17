f3edc1050cc79ac3d27adbdc43faf72dfbf65e0b
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-05-17 15:15:54 +0200

    two new pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me!</h1>
+      <p>Code Monkey in Berlin</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>How to find me...</h1>
+      <p>stephen.a.ingram@gmail.com</p>
+    </div>
+  )
+}
+
+export default ContactPage
