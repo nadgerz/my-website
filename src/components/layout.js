04bf1c7af1d6ada01e61c740c4282055862610c3
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-05-20 13:55:19 +0200

    Layout brings in the header and footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,16 @@
 import React from "react"
 
-const Layout = () => {
-  return <div />
+import Header from "../components/header"
+import Footer from "../components/footer"
+
+const Layout = props => {
+  return (
+    <div>
+      <Header />
+      {props.children}
+      <Footer />
+    </div>
+  )
 }
 
 export default Layout
