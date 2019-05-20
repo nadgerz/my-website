714a40275c752362479abe396df28ffd8af1f6fd
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-05-20 16:07:00 +0200

    CSS modules

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,7 @@ const Header = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link className="link" to="/">Home</Link>
           </li>
           <li>
             <Link to="/blog">Blog</Link>
