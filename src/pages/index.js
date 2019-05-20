76c15f23209c04a5b6154d52829ef70fbb0f3f7e
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-05-20 13:51:08 +0200

    simpler

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Header from "../components/header"
-import Footer from "../components/footer"
 import Layout from "../components/layout"
 
 const IndexPage = () => {
-  return <Layout />
-
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>Hello.</h1>
       <h2>I'm Steve, a fat bastard.</h2>
       <p>
-        Need a developer? <a href="/contact">Contact me.</a>
-      </p>
-      <p>
         Need a developer? <Link to="/contact">Contact me.</Link>
       </p>
-
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 
