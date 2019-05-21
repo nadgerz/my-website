f7db7249333a26a34b9d7bbeee22789e7b48771d
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-05-21 17:24:52 +0200

    createPages

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 
 const Blog = () => {
-  return <Layout>This is the Blog template.Blog</Layout>
+  return <Layout>This is the Blog template.</Layout>
 }
 
 export default Blog
