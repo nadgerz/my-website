ab2ed9e546fa11a49212ee6cd7ff7223371111ef
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-05-21 15:48:28 +0200

    bloggies

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -20,10 +20,27 @@ const BlogPage = () => {
     }
   `)
 
+  console.clear()
+  console.log(data)
+  console.log(typeof data)
+
   return (
     <Layout>
       <h1>My Blog.</h1>
       <p>Posts will show up here later on.</p>
+      {data.allMarkdownRemark.edges.map(node => {
+        console.log(node)
+        console.table(node)
+        console.log(typeof node)
+        const frontmatter = node.node.frontmatter
+
+        return (
+          <ol>
+            <h2>{frontmatter.title}</h2>
+            <p>{frontmatter.date}</p>
+          </ol>
+        )
+      })}
     </Layout>
   )
 }
