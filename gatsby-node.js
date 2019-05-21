5ab95aa3d0b100495e43da23eabe9f39eb00e142
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-05-21 16:43:54 +0200

    fine the slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,11 @@
+const path = require("path")
+
 module.exports.onCreateNode = ({ node, actions }) => {
   const { createNode, createNodeField } = actions
 
   if (node.internal.type === "MarkdownRemark") {
     console.log(JSON.stringify(node, null, 4))
+    const slug = path.basename(node.fileAbsolutePath, ".md")
+    console.log(slug)
   }
 }
