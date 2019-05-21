4b6db4fb7f7e894b9b04b583a5b13da77b5f9add
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-05-21 16:39:23 +0200

    Only MarkdownRemark

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 module.exports.onCreateNode = ({ node, actions }) => {
   const { createNode, createNodeField } = actions
 
-  console.log(node)
-  console.log(JSON.stringify(node, null, 4))
+  if (node.internal.type === "MarkdownRemark") {
+    console.log(JSON.stringify(node, null, 4))
+  }
 }
