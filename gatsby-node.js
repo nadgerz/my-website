a4a15d21bc362e79ac78563fa3f1e3c055b6ede9
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-05-21 16:45:49 +0200

    now graphql ready

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,15 @@
 const path = require("path")
 
 module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNode, createNodeField } = actions
+  const { createNodeField } = actions
 
   if (node.internal.type === "MarkdownRemark") {
-    console.log(JSON.stringify(node, null, 4))
     const slug = path.basename(node.fileAbsolutePath, ".md")
-    console.log(slug)
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
   }
 }
