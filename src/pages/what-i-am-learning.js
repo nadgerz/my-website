3829ade3b93ed52f19cb41c12300ef9053e13686
nadgerz <stephen.a.ingram@gmail.com>
2019-05-23 15:50:20 +0200

    stuff I use and am learning and want to learn

---
+++ src/pages/what-i-am-learning.js
@@ -0,0 +1,56 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Head from "../components/head"
+
+const WhatIAmLearningPage = () => {
+  return (
+    <Layout>
+      <Head title="WhatIAmLearning" />
+
+      <h1>What I Am Learning right now...</h1>
+
+      <ul>
+        <li>
+          <h2>Languages/Frameworks/Libraries</h2>
+          <ul>
+            <li>React</li>
+            <li>Go</li>
+            <li>Node.js</li>
+            <li>TypeScript</li>
+          </ul>
+        </li>
+
+        <li>
+          <h2>Tools</h2>
+          <ul>
+            <li>Docker</li>
+            <li>Jest</li>
+          </ul>
+        </li>
+      </ul>
+
+      <h1>What's up next...</h1>
+      <ul>
+        <li>
+          <h2>Languages/Frameworks/Libraries</h2>
+          <ul>
+            <li>Flutter/Dart</li>
+            <li>Electron</li>
+          </ul>
+        </li>
+
+        <li>
+          <h2>Tools</h2>
+          <ul>
+            <li>CI/CD</li>
+            <li />
+          </ul>
+        </li>
+      </ul>
+    </Layout>
+  )
+}
+
+export default WhatIAmLearningPage
--- src/pages/what-i-use.js
+++ src/pages/what-i-use.js
@@ -10,6 +10,44 @@ const WhatIUsePage = () => {
       <Head title="WhatIUse" />
 
       <h1>What I Use...</h1>
+
+      <li>
+        <h2>Languages/Frameworks/Libraries</h2>
+        <ul>
+          <li>JavaScript</li>
+          <li>Java</li>
+          <li>React</li>
+          <li>Go</li>
+          <li>Node.js</li>
+        </ul>
+      </li>
+
+      <li>
+        <h2>Editors/IDEs</h2>
+        <ul>
+          <li>vim</li>
+          <li>WebStorm</li>
+          <li>IntelliJ</li>
+        </ul>
+      </li>
+
+      <li>
+        <h2>Tools</h2>
+        <ul>
+          <li>Docker</li>
+          <li>Jest</li>
+          <li>Prettier</li>
+          <li>ESLint</li>
+        </ul>
+      </li>
+
+      <li>
+        <h2>Databases</h2>
+        <ul>
+          <li>PostgreSQL</li>
+          <li>mongodb</li>
+        </ul>
+      </li>
     </Layout>
   )
 }
