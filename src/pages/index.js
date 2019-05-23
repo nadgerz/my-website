8c92a3f619ad60604d87268126a7a2bfc136f6d6
nadgerz <stephen.a.ingram@gmail.com>
2019-05-23 14:19:00 +0200

    what is different?

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => {
 
       <h1>Hello.</h1>
 
-      <h2>I'm Steve, a fat bastard.</h2>
+      <h2>I'm Steve, a code monkey.</h2>
       <p>
         Need a developer? <Link to="/contact">Contact me!</Link>
       </p>
