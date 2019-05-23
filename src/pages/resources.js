d854ba2392489501ccb91c5371bf882e46ac4f81
nadgerz <stephen.a.ingram@gmail.com>
2019-05-23 15:54:44 +0200

    need to expand on these with proper links and details and logos

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -10,6 +10,18 @@ const ResourcesPage = () => {
       <Head title="Code" />
 
       <h1>Resources</h1>
+
+      <p>This is a list of resources I use/have used</p>
+
+      <ul>
+        <li>WesBos</li>
+        <li>JetBrains</li>
+        <li>egghead.io</li>
+        <li>frontendmasters.com</li>
+        <li>packtpub.com</li>
+        <li>Manning books</li>
+        <li>Pragmatic Programmer books</li>
+      </ul>
     </Layout>
   )
 }
