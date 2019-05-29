ffb3b480756764bcdcef41433e8a5e1f14d66a63
nadgerz <stephen.a.ingram@gmail.com>
2019-05-29 19:12:25 +0200

    typos

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,22 +15,22 @@ const AboutPage = () => {
         I am a Front-end/Back-end developer living and working in Berlin,
         Germany.
       </p>
-      <br /> <br />
+      <br />
       <p>
-        My early career was predominantly backend; Java/Sybase/Postgre, running
+        My early career was predominantly backend; Java/Sybase/Postgres, running
         on various *nix systems, but in recent years I have been pivoting to
         frontend languages and tooling, (React, TypeScript, Jest, ...) just
-        because the pace of change and invention there is more exciting for me.
+        because the pace of change and invention there is more exciting to me.
       </p>
-      <br /> <br />
+      <br />
       <p>
         That said, I haven't totally let go of the backend world. I am enjoying
         learning Go(lang), mongodb, Swift and all that DevOps goodness, with
         Docker, Kubernetes, Travis CI and friends.
       </p>
-      <br /> <br />
+      <br />
       <p>
-        My wish (hope?) is to work in a asmall to medium sized team where i can
+        My wish (hope?) is to work in a small to medium sized team where I can
         continue to learn and contribute to projects/applications using any (or
         all!) of the above.
       </p>
