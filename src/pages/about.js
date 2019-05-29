fcc04fd534426fe93cd268ceacba875f5513ee4d
nadgerz <stephen.a.ingram@gmail.com>
2019-05-29 23:59:51 +0200

    adding some stuff I am now looking at

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,27 +10,32 @@ const AboutPage = () => {
       <Head title="About" />
       <h1>About Me!</h1>
       <p>
-        I am a frontend/backend developer living and working in Berlin,
-        Germany.
+        I am a frontend/backend developer living and working in Berlin, Germany.
       </p>
       <br />
       <p>
-        My early career was predominantly backend; Java/Sybase/Postgres, running
-        on various *nix systems, but in recent years I have been pivoting to
-        frontend languages and tooling, (React, TypeScript, Jest, ...) just
-        because the pace of change and invention there is more exciting to me.
+        My early career was predominantly backend;
+        C/Java/python/Sybase/PostgreSQL/postgis, running on various *nix
+        systems, but in recent years I have been pivoting to frontend languages
+        and tooling, (React, TypeScript, Jest, ...) just because the pace of
+        change and invention there is more exciting to me.
       </p>
       <br />
       <p>
         That said, I haven't totally let go of the backend world. I am enjoying
-        learning Go(lang), mongodb, Swift and all that DevOps goodness, with
-        Docker, Kubernetes, Travis CI and friends.
+        learning Go(lang), mongodb, Swift, Node.js, Electron and all that DevOps
+        goodness, with Docker, Kubernetes, Travis CI and friends.
       </p>
       <br />
       <p>
         My wish (hope?) is to work in a small to medium sized team where I can
         continue to learn and contribute to projects/applications using any (or
         all!) of the above.
+      </p>
+      <br />
+      <p>TL;DR - Ha! You did! This is at the end.</p>
+      <p>
+        Life is short... coding is fun, but it is REALLY fun as part of a team.
       </p>
     </Layout>
   )
