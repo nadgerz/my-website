beae6493b2e32492a572bc19c299ad4f6da5f6c7
nadgerz <stephen.a.ingram@gmail.com>
2019-07-07 20:25:04 +0200

    fixing lists

--- src/pages/code.js
+++ src/pages/code.js
@@ -11,28 +11,30 @@ const CodePage = () => {
 
       <h1>Code</h1>
 
-      <li>
-        <h2>This Site!</h2>
-        <p>
-          This website was built using Gatsby, React, SCSS and a smattering of
-          Markdown and Contentful CMS goodness. It is based on the tutorial by
-          Andrew Mead.
-        </p>
-        <a href="https://github.com/nadgerz/gatsby-bootcamp" target="__blank">
-          [git]
-        </a>
-      </li>
+      <ul>
+        <li>
+          <h2>This Site!</h2>
+          <p>
+            This website was built using Gatsby, React, SCSS and a smattering of
+            Markdown and Contentful CMS goodness. It is based on the tutorial by
+            Andrew Mead.
+          </p>
+          <a href="https://github.com/nadgerz/gatsby-bootcamp" target="__blank">
+            [git]
+          </a>
+        </li>
 
-      <li>
-        <h2>Mini Mapper</h2>
-        <p>Placeholder for my next personal project.</p>
-        <p>
-          It will be a small mapping project to try and bring together some of
-          the languages, frameworks and tools I have been trying to learn; like
-          React/React-Native, Go(lang), mongodb, Docker, TypeScript.
-        </p>
-        <Link to={"/mini-mapper"}>[git]</Link>
-      </li>
+        <li>
+          <h2>Mini Mapper</h2>
+          <p>Placeholder for my next personal project.</p>
+          <p>
+            It will be a small mapping project to try and bring together some of
+            the languages, frameworks and tools I have been trying to learn;
+            like React/React-Native, Go(lang), mongodb, Docker, TypeScript.
+          </p>
+          <Link to={"/mini-mapper"}>[git]</Link>
+        </li>
+      </ul>
     </Layout>
   )
 }
