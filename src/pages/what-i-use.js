79e342f9a0a5f64396aa715ba33da90d205315cb
nadgerz <stephen.a.ingram@gmail.com>
2019-07-07 20:03:25 +0200

    fixing weird list

--- src/pages/what-i-use.js
+++ src/pages/what-i-use.js
@@ -10,44 +10,46 @@ const WhatIUsePage = () => {
 
       <h1>What I Use...</h1>
 
-      <li>
-        <h2>Languages/Frameworks/Libraries</h2>
-        <ul>
-          <li>JavaScript</li>
-          <li>Java</li>
-          <li>React</li>
-          <li>React-Native</li>
-          <li>Go(lang)</li>
-          <li>Node.js</li>
-        </ul>
-      </li>
+      <ul>
+        <li>
+          <h2>Languages/Frameworks/Libraries</h2>
+          <ul>
+            <li>JavaScript</li>
+            <li>Java</li>
+            <li>React</li>
+            <li>React-Native</li>
+            <li>Go(lang)</li>
+            <li>Node.js</li>
+          </ul>
+        </li>
 
-      <li>
-        <h2>Editors/IDEs</h2>
-        <ul>
-          <li>vim</li>
-          <li>WebStorm</li>
-          <li>IntelliJ</li>
-        </ul>
-      </li>
+        <li>
+          <h2>Editors/IDEs</h2>
+          <ul>
+            <li>vim</li>
+            <li>WebStorm</li>
+            <li>IntelliJ</li>
+          </ul>
+        </li>
 
-      <li>
-        <h2>Tools</h2>
-        <ul>
-          <li>Docker</li>
-          <li>Jest</li>
-          <li>Prettier</li>
-          <li>ESLint</li>
-        </ul>
-      </li>
+        <li>
+          <h2>Tools</h2>
+          <ul>
+            <li>Docker</li>
+            <li>Jest</li>
+            <li>Prettier</li>
+            <li>ESLint</li>
+          </ul>
+        </li>
 
-      <li>
-        <h2>Databases</h2>
-        <ul>
-          <li>PostgreSQL</li>
-          <li>mongodb</li>
-        </ul>
-      </li>
+        <li>
+          <h2>Databases</h2>
+          <ul>
+            <li>PostgreSQL</li>
+            <li>mongodb</li>
+          </ul>
+        </li>
+      </ul>
     </Layout>
   )
 }
