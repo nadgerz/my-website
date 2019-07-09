1151794c60d1ee95028a7a1aad1f2daf5e184ad9
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-07-09 22:44:43 +0200

    better link

--- src/pages/code.js
+++ src/pages/code.js
@@ -17,13 +17,13 @@ const CodePage = () => {
           <p>
             This website was built using Gatsby, React, SCSS and a smattering of
             Markdown and Contentful CMS goodness. It is based on the tutorial by
-            Andrew Mead called 'The Great gatsby Bootcamp', which you can find
+            Andrew Mead, which you can find @ 
             <a
               href="https://www.youtube.com/watch?v=8t0vNu2fCCM"
               target="__blank"
               rel="noopener noreferrer"
             >
-              here.
+              'The Great Gatsby Bootcamp'
             </a>
           </p>
           <a
