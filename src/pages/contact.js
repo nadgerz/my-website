341c153deb01ae4165b90edb9741d4ec9bc6f6b9
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-07-11 19:29:11 +0200

    changed h1 to h2, added <br>

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,42 +3,45 @@ import React from 'react';
 import Layout from '../components/layout';
 import Head from '../components/head';
 
+
 const ContactPage = () => {
   return (
     <Layout>
-      <Head title="Contact" />
-
-      <h1>How to contact me/Where to find me</h1>
-
-      <p>
-        <a
-          href="mailto:stephen.a.ingram@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          stephen.a.ingram@gmail.com
-        </a>
-      </p>
-
-      <p>
-        <a
-          href="https://www.linkedin.com/in/stephen-ingram-3078aa19/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-      </p>
-
-      <p>
-        <a
-          href="https://github.com/nadgerz"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </p>
+      <Head title="Contact"/>
+      
+      <div id={'contact'} className={'contact .container'}>
+        
+        <h2>How to contact me/<br/>Where to find me</h2>
+        <p>
+          <a
+            href="mailto:stephen.a.ingram@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            stephen.a.ingram@gmail.com
+          </a>
+        </p>
+        
+        <p>
+          <a
+            href="https://www.linkedin.com/in/stephen-ingram-3078aa19/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </p>
+        
+        <p>
+          <a
+            href="https://github.com/nadgerz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
+      </div>
     </Layout>
   );
 };
