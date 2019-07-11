ae09f4abc287428f123d79a54362020af3e74a39
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-07-11 19:23:18 +0200

    changed layout for 'what I am learning' page

--- src/pages/what-i-am-learning.js
+++ src/pages/what-i-am-learning.js
@@ -3,70 +3,73 @@ import React from 'react';
 import Layout from '../components/layout';
 import Head from '../components/head';
 
+
 const WhatIAmLearningPage = () => {
   return (
     <Layout>
-      <Head title="What I Am Learning" />
-
-      <h1>What I Am Learning right now...</h1>
-
-      <ul>
-        <li>
-          <h2>Languages/Frameworks/Libraries</h2>
-          <ul>
-            <li>React</li>
-            <li>React-Native</li>
-            <li>Go</li>
-            <li>Node.js</li>
-            <li>TypeScript</li>
-            <li>Kotlin</li>
-          </ul>
-        </li>
-
-        <li>
-          <h2>Tools</h2>
-          <ul>
-            <li>Docker</li>
-            <li>Jest</li>
-          </ul>
-        </li>
-      </ul>
-
-      <hr />
-
-      <h1>What's up next...</h1>
-      <ul>
-        <li>
-          <h2>Languages/Frameworks/Libraries</h2>
-          <ul>
-            <li>Flutter/Dart</li>
-            <li>Swift</li>
-          </ul>
-        </li>
-
-        <li>
-          <h2>Tools</h2>
-          <ul>
-            <li>CI/CD</li>
-          </ul>
-        </li>
-      </ul>
-
-      <hr />
-
-      <h1>Resources</h1>
-
-      <p>This is a list of resources I use/have used</p>
-
-      <ul>
-        <li>WesBos</li>
-        <li>JetBrains</li>
-        <li>egghead.io</li>
-        <li>frontendmasters.com</li>
-        <li>packtpub.com</li>
-        <li>Manning books</li>
-        <li>Pragmatic Programmer books</li>
-      </ul>
+      <Head title="What I Am Learning"/>
+      
+      <div id={'what-im-learning'} className={'what-im-learning'}>
+        
+        <h2>What I Am Learning right now...</h2>
+        
+        <ul className={'learning-now'}>
+          <li className={'flex-item'}>
+            <h3>Languages/<br/>Frameworks/<br/>Libraries</h3>
+            <ul>
+              <li>React</li>
+              <li>React-Native</li>
+              <li>Go</li>
+              <li>Node.js</li>
+              <li>TypeScript</li>
+              <li>Kotlin</li>
+            </ul>
+          </li>
+          
+          <li className={'flex-item'}>
+            <h3>Tools</h3>
+            <ul>
+              <li>Docker</li>
+              <li>Jest</li>
+            </ul>
+          </li>
+        </ul>
+        
+        <hr/>
+        
+        <h2>What's up next...</h2>
+        
+        <ul className={'up-next flex-item'}>
+          <li className={'flex-item'}>
+            <h3>Languages/<br/>Frameworks/<br/>Libraries</h3>
+            <ul>
+              <li>Flutter/Dart</li>
+              <li>Swift</li>
+            </ul>
+          </li>
+          
+          <li className={'flex-item'}>
+            <h3>Tools</h3>
+            <ul>
+              <li>CI/CD</li>
+            </ul>
+          </li>
+        </ul>
+        <hr/>
+        
+        <h2>Resources</h2>
+        <p>This is a list of resources I use/have used</p>
+  
+        <ul className={'resources'}>
+          <li>WesBos</li>
+          <li>JetBrains</li>
+          <li>egghead.io</li>
+          <li>frontendmasters.com</li>
+          <li>packtpub.com</li>
+          <li>Manning books</li>
+          <li>Pragmatic Programmer books</li>
+        </ul>
+      </div>
     </Layout>
   );
 };
